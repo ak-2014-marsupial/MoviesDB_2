fad33048b7b0c07ba16c5f93e263934f5bc39811
Andrew <ak36794ca@gmail.com>
2024-06-20 14:34:23 +0300

    change LangSwitcher, fixed GUI

--- src/components/LangSwitcher/LangSwitcher.tsx
+++ src/components/LangSwitcher/LangSwitcher.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useTranslation} from "react-i18next";
 
 import css from "./LangSwitcher.module.css"
@@ -6,36 +6,32 @@ import {Button} from "../Button";
 import {useAppMergeParamsWithSearchParams} from "../../hooks/useAppMergeParamsWithSearchParams";
 
 
-const locales: Record<string, { title: string }> = {
-    "en-US": {title: "En"},
-    "uk-UK": {title: "Укр"},
+const locales: Record<string, { value: string, title: string, next: string }> = {
+    "en-US": {value: "en-US", title: "En", next: "uk-UK"},
+    "uk-UK": {value: "uk-UK", title: "Uk", next: "en-US"},
 }
+
+
 const LangSwitcher = () => {
     const {mergeParamsWithSearchParams} = useAppMergeParamsWithSearchParams();
-
+    const initLocale = localStorage.getItem("locale") || "uk-UK";
+    const [locale, setLocale] = useState(initLocale);
     const {i18n, t} = useTranslation();
 
-    const handleClick = (locale: string) => {
-        mergeParamsWithSearchParams({"lang": locale})
-        i18n.changeLanguage(locale)
+    if (i18n.resolvedLanguage !== locale) {
+        i18n.changeLanguage(locale);
+    }
+
+    const handleClick = () => {
+        const nextLocale=locales[locale].next;
+        mergeParamsWithSearchParams({"lang": nextLocale})
+        i18n.changeLanguage(nextLocale);
+        setLocale(nextLocale);
     }
 
     return (
-        <div className={css.dropdown}>
-            {t("lang")}
-            <div className={css.dropdown_content}>
-                {Object.keys(locales).map((locale: string) => (
-                    <Button key={locale}
-                            className={css.langSwitcher}
-                        // style={{fontWeight: i18n.resolvedLanguage === locale ? 'bold' : 'normal'}}
-                            type="submit"
-                            onClick={() => handleClick(locale)}>
-                        {locales[locale].title}
-                    </Button>
-                ))
-
-                }
-            </div>
+        <div className={css.lang_switcher}>
+            <Button onClick={handleClick} className={css.btn}>{locales[locale].title}</Button>
         </div>
 
 
